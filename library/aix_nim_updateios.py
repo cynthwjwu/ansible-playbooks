a3fbe0d923c8648c3ad651c28f46fa6a4125aa17
cynthwjwu <cynthwjwu@gmail.com>
2017-07-27 15:31:17 -0500

    parameter checking

--- library/aix_nim_updateios.py
+++ library/aix_nim_updateios.py
@@ -1,48 +1,57 @@
 #!/usr/bin/python
 #
-# Copyright 2017, International Business Machines Corporation
+# Copyright (c) 2017, IBM Corp
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# AIX NIM module for Ansible :
+# https://github.com/ansible-aix/patch_mgmt
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# This file is part of Ansible,
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 ############################################################################
 
-from ansible.module_utils.basic import *
 import os
-import glob
-import subprocess
-import logging
 import re
+import glob
 import shutil
+import subprocess
 import threading
+import logging
 import time
+
+# Ansible module 'boilerplate'
+from ansible.module_utils.basic import *
 
 
 DOCUMENTATION = """
 ---
 module: update_ios
-short_description: Update the ios
+authors: Cynthia Wu, Marco Lugo
+short_description: Perform a VIO update 
+
 """
 
+
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-def exec_cmd(module, cmd):
+def exec_cmd(cmd):
     """
     Execute the given command
-        - module    the module variable
         - cmd       array of command parameters
 
-    In case of error set an error message and fail the module
+    In case of error return (1, " ")
 
     return
         - ret_code  (0)
@@ -58,14 +67,9 @@ def exec_cmd(module, cmd):
     try:
         std_out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as exc:
-        msg = 'Command: {} Exception.Args{} =>Data:{} ... Error :{}'. \
-            format(cmd, exc.cmd, exc.output, exc.returncode)
-        module.fail_json(msg=msg)
         return (1, " ")
     except Exception as exc:
-        msg = 'Command: {} Exception.Args{} =>Data:{} ... Error :{}'. \
-            format(cmd, exc.args, std_out, std_err)
-            return (1, " ")
+        return (1, " ")
 
     # DEBUG
     DEBUG_DATA.append('exec command:{}'.format(cmd))
@@ -76,49 +80,151 @@ def exec_cmd(module, cmd):
 
     return (0, std_out)
 
+
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-def get_command(module):
+def check_lpp_source(module, lpp_source):
+    """
+    Check to make sure lpp_source exists
+        - module        the module variable
+        - lpp_source    lpp_source param provided by module
+
+    In case lpp_source does not exist fail the module 
+
+    return
+        - exists        True
+    """
+
+    # find location of lpp_source
+    cmd = ['lsnim', '-a', 'location', lpp_source]
+    ret, std_out = exec_cmd(cmd)
+    if ret != 0:
+        logging.error('NIM - Error: cannot find location of lpp_source {}'.format(lpp_source))
+        module.fail_json(msg="NIM - Error: cannot find location of lpp_source {}".format(lpp_source))
+    location = std_out.split()[3]
+
+    # check to make sure path exists
+    cmd = ['/bin/find/', location]
+    ret, std_out = exec_cmd(cmd)
+    if ret != 0:
+        logging.error('NIM - Error: cannot find location of lpp_source {}'.format(lpp_source))
+        module.fail_json(msg="NIM - Error: cannot find location of lpp_source {}".format(lpp_source))
+
+    return True
+
+
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+def check_targets(module, targets):
+    """
+    Check to make sure targets exist
+        - module        the module variable 
+        - targets       targets param provided by module
+
+    In case target does not exist fail the module 
+
+    return
+        - exists        target exists
+    """
+
+    cmd = ['lsnim','-l', targets]
+    ret, std_out = exec_cmd(cmd)
+    if ret != 0:
+        logging.error('NIM - Error: target {} cannot be found'.format(targets))
+        module.fail_json(msg="NIM - Error: target {} cannot be found".format(targets))
+
+    return True
+
+
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+def check_updateios_flags(module, flag):
+    """
+    Check to make sure updateios_flags value is valid
+        - module        the module variable
+        - flag          updateios action
+
+    return
+        - valid         True
+    """
+    valid_flags = ['-install', '-commit', '-reject', '-cleanup', 'remove']
+    if flag not in valid_flags:
+        logging.error('NIM - Error: updateios_flags parameter {} invalid'.format(flag))
+        module.fail_json(msg="NIM - Error: updateios_flags parameter {} invalid".format(flag))
+    logging.info('Updateios action: {}'.format(flag))
+
+    return True 
+
+
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+def get_updateios_cmd(module):
+    """
+    Assemble the updateios command
+        - module        the module variable 
+
+    return
+        - cmd           array of the command parameters
+    """
+
     global OUTPUT
 
     cmd = ['nim', '-o', 'updateios']
 
+    # lpp source
     if module.params['lpp_source']:
-        cmd += ['-a', 'lpp_source=%s' %(module.params['lpp_source'])]
+        if (check_lpp_source(module, module.params['lpp_source'])):
+            cmd += ['-a', 'lpp_source=%s' %(module.params['lpp_source'])]
 
+    # accept licenses
     if module.params['accept_licenses']:
         cmd += ['-a', 'accept_licenses=%s' %(module.params['accept_licenses'])]
     else: #default
         cmd += ['-a', 'accept_licenses=yes']
 
+    # updateios flags
     if module.params['updateios_flags']:
-        cmd += ['-a', 'updateios_flags=%s' %(module.params['updateios_flags'])]
+        if (check_updateios_flags(module, module.params['updateios_flags'])):
+            cmd += ['-a', 'updateios_flags=%s' %(module.params['updateios_flags'])]
 
-        if module.params['updateios_flags'] == "-remove":
-            if module.params['filesets'] != "none":
-                cmd += ['-a', 'filesets=%s' %(module.params['filesets'])]
-            elif module.params['installp_bundle'] != "none":
-                cmd += ['-a', 'installp_bundle=%s' %(module.params['installp_bundle'])]
+            if module.params['updateios_flags'] == "-remove":
+                if module.params['filesets'] != "none":
+                    cmd += ['-a', 'filesets=%s' %(module.params['filesets'])]
+                elif module.params['installp_bundle'] != "none":
+                    cmd += ['-a', 'installp_bundle=%s' %(module.params['installp_bundle'])]
         else:
             logging.info('VIO UPDATE - filesets {} and installp_bundle {} have been discarded'.format(module.params['filesets'], module.params['installp_bundle']))
             OUTPUT.append('Any installp_bundle or filesets have been discarded')
 
+    # preview mode
     if module.params['preview']:
         cmd += ['-a', 'preview=%s' %(module.params['preview'])]
     else: #default
         cmd += ['-a', 'preview=yes']
 
-    cmd += [module.params['targets']]
+    # targets
+    if module.params['targets']:
+        if (check_targets(module, module.params['targets'])):
+            cmd += [module.params['targets']]
+
     return cmd
+
 
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 def nim_updateios(module):
+    """
+    Execute the updateios command
+        - module        the module variable
+
+    return
+        - ret           return code of nim updateios command 
+    """
     global CHANGED
     global OUTPUT
 
-    cmd = get_command(module)
-    ret, std_out = exec_cmd(module, cmd)
+    cmd = get_updateios_cmd(module)
+    ret, std_out = exec_cmd(cmd)
 
     logging.info('[RC] {}'.format(ret))
     logging.info('[STDOUT] {}'.format(std_out))
@@ -129,6 +235,7 @@ def nim_updateios(module):
 
     OUTPUT.append("SUCCESS")
     CHANGED = True
+
     return ret
 
 
@@ -156,8 +263,11 @@ if __name__ == '__main__':
     LOGFRMT = '[%(asctime)s] %(levelname)s: [%(funcName)s:%(thread)d] %(message)s'
     logging.basicConfig(filename=LOGNAME, format=LOGFRMT, level=logging.DEBUG)
 
+
     logging.debug('*** START VIO UPDATE OPERATION ***')
     nim_updateios(module)
+    
+    logging.info('Done with nim updateios operation')
 
     try:
         module.exit_json(
